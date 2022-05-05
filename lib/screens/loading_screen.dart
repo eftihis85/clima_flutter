fb8df48fd6809792746fc112f81e9a5c22a2a864
Efty <eftihis@gmail.com>
2022-05-05 20:31:32 +0200

    lesson 140 completed

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Position? position;
+
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    if (permission != LocationPermission.denied ||
+        permission != LocationPermission.deniedForever) {
+      position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    }
+    print('Position is not assigned!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: TextButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
---
+++ lib/utilities/location_misc.dart
@@ -0,0 +1,7 @@
+import 'package:geolocator/geolocator.dart';
+
+// bool isLocationPersmionGranded() async {
+//   LocationPermission permission = await Geolocator.requestPermission();
+
+//   return true;
+// }
